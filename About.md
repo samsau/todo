d3db8d3048ec434d5b35d9f359c8bc31f7c30470
Saurabh Kumar <1355saurabh@gmail.com>
2024-03-05 07:26:50 +0530

    about the project file added.

---
+++ About.md
@@ -0,0 +1,10 @@
+This is a React Project with the following:
+
+- no database is added
+- it uses redux
+- tailwind is the CSS used
+
+###
+
+In the redux folder all the methods or actions are defined.
+In the components folder all the components are defined. - Todo.jsx is the head component in this folder
--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,7 +1,6 @@
 export const ADD_TODO = "ADD_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
-// export const DELETE_TODO = "DELETE_TODO";
 export const MARK_COMPLETE = "MARK_COMPLETE";
 export const MARK_INCOMPLETE = "MARK_INCOMPLETE";
 export const FILTER_TODO = "FILTER_TODO";
